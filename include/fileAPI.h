f5f16d4af164d84cb77e4cb88165a5ff648ca3c0
jpmerc <jean-philippe.mercier.2@ulaval.ca>
2014-06-20 12:54:22 -0400

    fileAPI beginning

---
+++ include/fileAPI.h
@@ -0,0 +1,36 @@
+#ifndef fileAPI_H
+#define fileAPI_H
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+using namespace std;
+
+struct Object{
+    string name;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr object_pointcloud;
+    geometry_msgs::PoseStampedConstPtr relative_arm_pose;
+    geometry_msgs::PoseStampedConstPtr object_pose;
+};
+
+
+class fileAPI{
+public:
+    fileAPI(string url);
+    Object createObject(string name,pcl::PointCloud<pcl::PointXYZRGB>::Ptr object_pointcloud,geometry_msgs::PoseStampedConstPtr relative_arm_pose,geometry_msgs::PoseStampedConstPtr object_pose);
+    void saveObject(Object obj);
+    void save(string name,pcl::PointCloud<pcl::PointXYZRGB>::Ptr object_pointcloud,geometry_msgs::PoseStampedConstPtr relative_arm_pose,geometry_msgs::PoseStampedConstPtr object_pose);
+    vector<Object> getAllObjects();
+    Object getObjectByIndex(int index);
+
+private:
+    void parseDirectory(); //Set the index number (all files will be of the form index.txt -> check the highest index and assign to index variable)
+
+    int highest_index;
+
+};
